cc065f5c1c428f75b4217b808035ac0311c61e3d
Marat Zainullin <maratreason@mail.ru>
2020-05-21 18:19:47 +0400

    change

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import createSagaMiddleWare from "redux-saga";
+import { applyMiddleware, compose, createStore } from 'redux'
+import createSagaMiddleWare from 'redux-saga'
 
-import rootReducer from "./reducers";
-import watchers from "./sagas";
+import rootReducer from './reducers'
+import watchers from './sagas'
 
 const composeEnhanters =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose
+  process.env.NODE_ENV === 'development'
+    ? // ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      compose
+    : null
 
 const sagaMiddleware = createSagaMiddleWare()
 
 const store = createStore(
   rootReducer,
-  composeEnhanters(applyMiddleware(sagaMiddleware)),
+  composeEnhanters(applyMiddleware(sagaMiddleware))
 )
 
 sagaMiddleware.run(watchers)
